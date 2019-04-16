f1bd8e8d221970382a0744954143b89d1bdbe354
Randy <randybruno424@gmail.com>
2019-04-16 11:28:57 -0400

    removed unused function args_parse from query.py

--- query.py
+++ query.py
@@ -1,5 +1,4 @@
 import sys
-import argparse
 import sqlite3
 
 class Query:
@@ -42,6 +41,7 @@ class Query:
         values = list(selector.fetchone())
         for label,value in zip(labels,values):
             print(label+": "+value)
+        return label+": "+value
 
         #if verbose == "True":
         #    print(f"Number of rows: {numrows} (not counting row of labels)")
@@ -49,25 +49,6 @@ class Query:
         #    print(' '.join(field for field in fields[0]))
 
 
-    def parse_args(self,args):
-        """ Configure parsing of command line arguments """
-        parser = argparse.ArgumentParser(description=(f"Prints details corresponding to ticker"
-            " and specific time"))
-        parser.add_argument("-verbose", "-v", nargs='?', default=None, type=str, 
-            help="when True, the number of rows and number of columns in the information"
-            " file will be printed out as well as the names of the columns.")
-
-        parser.add_argument("-file", nargs='?', default=None, type=str, 
-            help="information filename")
-        parser.add_argument("-ticker", nargs='?', default=None, type=str, 
-            help="ticker name")
-        parser.add_argument("-time", nargs='?', default=None, type=str, 
-            help="requested time")
-
-        args = parser.parse_args(args)
-        return args
-
-
 if __name__ == "__main__":
-    query = Query("01:00","test.db","ADAP")
+    query = Query("11:25","test.db","ADAP")
     query.find_symbol()
