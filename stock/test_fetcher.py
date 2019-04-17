64314f74e252673090b1c28b9f9320bec34044e6
Randy <randybruno424@gmail.com>
2019-04-17 14:41:45 -0400

    bug fix, query class

--- stock/test_fetcher.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(THIS_FOLDER+'/../')
-import fetcher
-
-def test_fetcher():
-    fetch = fetcher.Fetcher(10,"test.db")
-    fetch.fetch_all_data()
--- stock/test_query.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(THIS_FOLDER+'/../')
-import query as q
-
-def test_query():
-    query = q.Query("01:00","test.db","ADAP")
-    query.find_symbol()
